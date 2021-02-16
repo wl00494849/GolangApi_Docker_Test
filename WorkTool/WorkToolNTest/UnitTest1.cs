6e68f68f35bb51148f3b7705732356f42c6c3f2a
zhung1oz7 <55905379+wl00494849@users.noreply.github.com>
2021-02-16 15:28:12 +0800

    gitignore

--- WorkTool/WorkToolNTest/UnitTest1.cs
+++ WorkTool/WorkToolNTest/UnitTest1.cs
@@ -2,6 +2,7 @@ using NUnit.Framework;
 using NSubstitute;
 using WorkTool.Controllers;
 using WorkTool.Models.DataModel;
+using WorkTool.Services;
 using WorkTool.Interface;
 using WorkTool.Models;
 
@@ -10,21 +11,30 @@ namespace WorkToolNTest
     public class Tests
     {
         HomeController home;
-        
+        private ISqlClient _sqlClient;
+        private IUntityFunction _untity;
+        private WorkToolEntity _db;
+
+        public object Substitute { get; private set; }
+
         [SetUp]
         public void Setup()
         {
-            var sql = Substitute.For();
-            home = new HomeController();
+            _sqlClient = Substitute.For<ISqlClient, SqlClient>();
+            _untity = Substitute.For<IUntityFunction, UntityFunction>();
+            _db = Substitute.For<WorkToolEntity>();
+            home = new HomeController(_sqlClient,_db,_untity);
         }
 
         [Test]
-        public void Test1()
+        public void CreateWorkTest()
         {
             home.CreateWork(new Work()
             {
-                
+                WorkID = "12345678910",
+                WorkContents = "eqweqwe",
             });
+
             Assert.Pass();
         }
     }
