ac6dd9557aceb20e5eb274c912b9ef7f2eedb8d9
zhung1oz7 <55905379+wl00494849@users.noreply.github.com>
2021-04-14 16:15:37 +0800

    微調

--- WorkTool/WorkTool/Models/BaseResultModel.cs
+++ WorkTool/WorkTool/Models/BaseResultModel.cs
@@ -7,8 +7,11 @@ namespace WorkTool.Models
 {
     public class BaseResultModel<T>
     {
+        Enum.Response _response;
         public T body { get; set; }
-        public string message { get; set; }
+        public string message => _response.ToString();
+        public string code => Convert.ToInt32(_response).ToString().PadLeft(3, '0');
+        public Enum.Response response { set { _response = value; } get { return _response; } }
         public string exception { get; set; }
     }
 }
