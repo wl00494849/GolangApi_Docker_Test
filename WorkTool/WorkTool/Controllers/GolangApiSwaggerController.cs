eba996377b53aafef325306414e6428c5e0adebf
zhung1oz7 <55905379+wl00494849@users.noreply.github.com>
2021-05-09 23:47:47 +0800

    架構調整

--- WorkTool/WorkTool/Controllers/GolangApiSwaggerController.cs
+++ WorkTool/WorkTool/Controllers/GolangApiSwaggerController.cs
@@ -5,19 +5,21 @@ using System.Net.Http;
 using System.IO;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using WorkTool.Untity;
 
 namespace WorkTool.Controllers
 {
     public class GolangApiSwaggerController : Controller
     {
         [HttpPost("CallDockerTestApi")]
-        public string CallDockerTestApi()
+        public string CallDockerTestApi(string url)
         {
+            Uri uri = new Uri(string.IsNullOrEmpty(url) ? new DockerUrl().DockerTest : url);
             var str = "";
             using (var client = new HttpClient())
             {
                 var content = new StringContent("");
-                var response = client.GetAsync("http://localhost:8787/DockerTest");
+                var response = client.GetAsync(uri);
                 if (response != null)
                 {
                     Task task = response.Result.Content.ReadAsStreamAsync().ContinueWith(t =>
---
+++ WorkTool/WorkTool/Untity/DockerUrl.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WorkTool.Untity
+{
+    public class DockerUrl
+    {
+           public string DockerTest = "http://localhost:8787/DockerTest";
+    }
+}
